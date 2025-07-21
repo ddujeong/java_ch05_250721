ab4c1e4198b3c0679d39535808d4d8840bad0977
ddujeong <qoddl159@nate.com>
2025-07-21 18:15:29 +0900

    Test05 주석 추가

--- src/exam/Test05.java
+++ src/exam/Test05.java
@@ -4,6 +4,8 @@ public class Test05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 아래 숫자들을 모두 곱한 값을 배열을 사용하여 출력하시오
+		//1 2 3 4 5
 		int[] num = {1, 2, 3, 4, 6};
 		int sum = 1;
 		for (int i = 0; i < num.length; i++) {
